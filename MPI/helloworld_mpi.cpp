444f2d899f4fb7da9734f46878964ba5db9480a6
Karkusha23 <min.rinat567321@gmail.com>
2024-02-28 21:47:22 +0300

    Edit MPI hello world

--- MPI/helloworld_mpi.cpp
+++ MPI/helloworld_mpi.cpp
@@ -6,7 +6,11 @@ int main(int argc, char* argv[])
 	int ProcessRank, ProcessNum;
 
 	MPI_Init(&argc, &argv);
+
+	// Get process index
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcessRank);
+
+	// Get number of processes
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcessNum);
 
 	std::cout << "Hello world! from process " << ProcessRank << ". Total processes: " << ProcessNum << std::endl;
