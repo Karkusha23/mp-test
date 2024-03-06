1011de92e8244cea4328f32a6e6bd32ca3482203
Karkusha23 <min.rinat567321@gmail.com>
2024-03-06 17:43:28 +0300

    Add files via upload

---
+++ CUDA/1/kernel.cu
@@ -0,0 +1,16 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <string>
+
+__global__ void helloWorld()
+{
+    printf("Hello world! from thread %d\n", threadIdx.x);
+}
+
+int main(int argc, char* argv[])
+{
+    helloWorld<<<1, argc < 2 ? 5 : std::stoi(argv[1])>>>();
+    return 0;
+}
